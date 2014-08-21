a9fadc3327cbc4242e5a9575caf27ef9e0e8006f
Sherry Favors <sfavors3@gmail.com>
2014-08-21 04:27:17 -0400

    Sherry Favors-completedAssignment2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,33 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+##  This function accepts a matrix object as a parameter, inverts the object,
+##  and stores it in a cached environment
 
 makeCacheMatrix <- function(x = matrix()) {
+  m <- NULL                                 ## m is set to NULL each time makeCacheMatrix is called                                              
+  set <- function(y) {                      ## set(internal method): stores the object to the cached environment
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x                       ## get, setsolve, and getsolve (internal methods) allow external access to object
+  setsolve <- function(solve) m <<- solve
+  getsolve <- function() m
+  list(set = set, get = get,               ## list all internal methods
+       setsolve = setsolve,                
+       getsolve = getsolve)                
+}                                           
 
-}
+## This function returns the object created from the makeCacheMatrix function
+## and inverts the object.  If the object is already cached, the cached object
+## will be returned, otherwise, the matrix will be inverted and stored in the
+## cached environment before being returned
 
-
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cachesolve <- function(x, ...) {
+  m <- x$getsolve()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data, ...)
+  x$setsolve(m)
+  m
 }
